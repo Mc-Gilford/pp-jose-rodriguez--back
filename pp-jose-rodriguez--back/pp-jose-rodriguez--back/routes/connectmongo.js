fac11484d7c3c2400afad71dbb6af9edfde3e14a
McGilfordJose <34671406+McGilfordJose@users.noreply.github.com>
2021-03-30 04:13:27 -0600

    new

--- pp-jose-rodriguez--back/pp-jose-rodriguez--back/routes/connectmongo.js
+++ pp-jose-rodriguez--back/pp-jose-rodriguez--back/routes/connectmongo.js
@@ -5,7 +5,7 @@ async function main() {
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://BEDUDATA:Jesus330$@cluster0.v70yw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://BEDUDATA:Contra$@cluster0.v70yw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 
     const client = new MongoClient(uri);
